092f2de1e5dee0a9ae19a650ea1d55fa63ecfae6
wartox12811 <wiktor1234567809@gmail.com>
2020-05-07 10:38:25 +0200

    Edited name from Polish to English

---
+++ Files/Clicker.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "Ustaw.h"
+#include "Punkty.h"
+#include <time.h>
+#include <cstdlib>
+
+int main()
+{
+    srand(time(0));
+    SetOptions set;
+    Points add;
+    set.isTrue();
+    add.is();
+    add.add();
+}
